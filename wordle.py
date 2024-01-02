e78e7ac72aec803d2b952b1454284baaa0b5ad0d
sarcastic-pig <1005145q@gmail.com>
2024-01-01 20:49:13 -0600

    updated guessing system

--- wordle.py
+++ wordle.py
@@ -12,35 +12,46 @@ def get_word():
     print(secret_word)
     return secret_word
 
-def split_word(x):
-    return list(x)
-    
-#print(split_word("words"))
 
 answer = get_word()
-# ans_lst = split_word(answer)
+word_disp = ["_", "_", "_", "_", "_"]
+letter_disp = []
 guesses = 0
+
+
 while guesses < 6:
     guess = input("Guess a word: ").lower()
-    # guess_lst = split_word(guess)
     if len(guess) == 5 and guess in wrd_bank:
             if guess == answer:
                 print("correct")
                 print(f"You guessed the word in {guesses + 1} guesses!")
                 break
             else:
+                #want to print out _____ with correct guesses inserted
                 for i in range(5):
                     #check if letters in guess are in same spot as word
                     if guess[i] == answer[i]:
-                        print(f"{guess[i]} is correct")
+                        if word_disp[i] == "_":
+                                word_disp[i] = guess[i]
+                        #print(f"{guess[i]} is correct")
                     #check if letters in guess are in word
+                    
+                    #list does not need to populate if letter is in word_disp UNLESS there is a repeat letter 
+                
+                    if guess[i] in letter_disp:
+                        continue
                     elif guess[i] in answer:
-                        print(f"{guess[i]} is in word")
-
-                #print("incorrect")
+                        if guess[i] != answer[i] and guess[i] not in word_disp[i]:
+                            letter_disp.append(guess[i])
+                    
+    
+                        #print(f"{guess[i]} is in word")
+                print(' '.join(word_disp))
+                print(' '.join(letter_disp))
                 guesses += 1
+                print(f"You have {6 - guesses} guesses left.")
     else:
         if len(guess) != 5:
             print("Guess must be 5 letters")
         elif guess not in wrd_bank:
-            print("Guess must be real word")
+            print("Word is not in word list")
